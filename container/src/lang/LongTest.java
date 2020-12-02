bd70eb59951ca5dbab3f27c32f608b2de0ff7dc9
dongxiaosong <dxsjl520@163.com>
2020-12-02 19:20:13 +0800

    66

--- container/src/lang/LongTest.java
+++ container/src/lang/LongTest.java
@@ -3,5 +3,6 @@ package lang;
 public class LongTest {
     public static void main(String[] args) {
         System.out.println(Long.toUnsignedString(-16,10));
+        System.out.println(55555);
     }
 }
